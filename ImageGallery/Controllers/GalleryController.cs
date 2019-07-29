9e2876e5f920f9d96e3af090e08554a117b5bade
Denis Sidorov <dsigmat@hotmail.com>
2019-07-29 15:44:34 +0600

    Add project files.

---
+++ ImageGallery/Controllers/GalleryController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageGallery.Data;
+using ImageGallery.Interface;
+using ImageGallery.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageGallery.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IUploadInterface _upload;
+
+        public GalleryController(ApplicationDbContext context, IUploadInterface upload)
+        {
+            _context = context;
+            _upload = upload;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var getDetailPicture = _context.ImageDetail.Include(i => i.Category).ToList().OrderByDescending(a => a.ReleaseDate);
+            return View(getDetailPicture);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var image = from i in _context.ImageDetail.Include(p => p.Category)
+            select i;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                image = image.Where(s => s.ImageName.Contains(searchString));
+            }
+
+            return View(await image.ToListAsync());
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pictureDetail = await _context.ImageDetail
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pictureDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(pictureDetail);
+        }
+
+        
+    }
+}
---
+++ ImageGallery/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ImageGallery.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageGallery.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options){}
+
+        public DbSet<ImageDetail> ImageDetail { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
